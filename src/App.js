ea01c60dfc8fcff7d42dcdfc05a2a1fb329b13ba
Vira Vznk <veravznk@gmail.com>
2026-04-13 23:18:11 +0300

    2 first comm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
   useEffect(() => {
-    fetch("/api/test")
-      .then(res => res.json())
-      .then(data => setProducts(data));
-  }, []);
-
-  return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((p, i) => (
-          <li key={i}>{p}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  fetch("/api/test")
+    .then(res => res.text())
+    .then(data => console.log(data));
+    
+}, []);
 }
 
 export default App;
